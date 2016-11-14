ded495627e0e94d1b6dc742207ecd849f55c5f80
Koncealed <robbieskonieczny@gmail.com>
2016-11-14 16:31:49 -0500

    Create DiagonalDiffrence.java

---
+++ DiagonalDiffrence.java
@@ -0,0 +1,45 @@
+/*                     https://www.hackerrank.com/challenges/diagonal-difference                    */    
+
+
+import java.util.*;
+
+class diffrence {
+	public static int getDiffrence(String[] input,int size) {
+		int rtl = 0;
+		int ltr = 0;
+		int[][] graph = new int[size][size];
+		int count = graph.length - 1;
+		for(int i = 0; i < size;++i) {
+			String[] input1 = input[i].split(" ");
+			for(int j = 0; j < size;++j) {
+				graph[i][j] = Integer.parseInt(input1[j]);
+			}
+		}
+		for(int i = 0; i < size;++i) {
+			ltr += graph[i][i];
+			rtl += graph[i][count];
+			count--;
+		}
+		int result = ltr - rtl;
+		
+		return Math.abs(result);
+	}
+}
+
+
+public class Solution {
+	
+
+	public static void main(String[] args) {
+		int amount = 0;
+		Scanner scan = new Scanner(System.in);
+		amount = scan.nextInt();
+		scan.nextLine();
+		String[] input = new String[amount];
+		for(int i = 0; i < amount;i++){
+			input[i] = scan.nextLine();
+		}
+		System.out.println(diffrence.getDiffrence(input, amount));
+	}
+
+}
