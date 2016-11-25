38233827fcf4378f6f19300a1a0b02d083c28e77
Koncealed <robbieskonieczny@gmail.com>
2016-11-24 22:15:07 -0500

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -7,7 +7,7 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int amount = input.nextInt();
-    input.nextLine(); // HAS TO GET RID OF \N
+                input.nextLine(); // HAS TO GET RID OF \N
 		String nums = input.nextLine();
 		String[] numbers = nums.split(" ");
 		input.close();
