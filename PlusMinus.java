bce2e59e70ec321f643a5a87882e48381737906f
Koncealed <robbieskonieczny@gmail.com>
2016-11-27 22:39:00 -0500

    Update PlusMinus.java

--- PlusMinus.java
+++ PlusMinus.java
@@ -1,29 +1,22 @@
 import java.util.Scanner;
-
 class plusMinus {
 	public static double[] array(int size,double[] array) {
-		/*    [0] == is postive numbers
-	        [1] is negative numbers
-	        [2] all 0's 
-	         */
 		double[] newArray = new double[3];
 		for(int i = 0; i < size;++i) {
 			if(array[i] == 0) {
 				++newArray[2];
 			} else if(array[i] > 0) {
 				++newArray[0];
-			} else if(array[i] < 0) {
+			} else {
 				++newArray[1];
 			}
 		}
-		newArray[0] = newArray[0] / size;
-		newArray[1] = newArray[1] / size;
-		newArray[2] = newArray[2] / size;
+		for(int i = 0; i < newArray.length;++i) {
+			newArray[i] /= size;
+		}
 		return newArray;
 	}
 }
-
-
 
 public class Solution {
 
@@ -32,14 +25,14 @@ public class Solution {
 		int size = scan.nextInt();
 		scan.nextLine();
 		String temp = scan.nextLine();
+		scan.close();
 		String[] temp1 = temp.split(" ");
 		double[] array = new double[size];
 		for(int i = 0; i < size;++i) {
 			array[i] = Double.parseDouble(temp1[i]);
 		}
-		System.out.println(plusMinus.array(size, array)[0]);
-		System.out.println(plusMinus.array(size, array)[1]);
-		System.out.println(plusMinus.array(size, array)[2]);
-		
+		for(int i = 0; i < 3;++i) {
+			System.out.println(plusMinus.array(size, array)[i]);
+		}
 	}
 }
