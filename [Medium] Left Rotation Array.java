51701616c840c59bdef63385fe6e51e772368ff8
Koncealed <robbieskonieczny@gmail.com>
2016-12-01 16:59:22 -0500

    Update [Medium] Left Rotation Array.java

--- [Medium] Left Rotation Array.java
+++ [Medium] Left Rotation Array.java
@@ -1,15 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class rotate {
 	public static int[] left(String input, int rotateAmount, int size) {
 		int[] rotated = new int[size];
-		int[] temp1 = new int[size];
 		String[] temp = input.split(" ");
-		for(int i = 0; i < size;++i) {
-			for(int j = 0; j < size;++j) {
-				temp1[i] = Integer.parseInt(temp[i]);
-			}
-		}
 		for(int i = 0; i < size;++i) { // Control the number placement we are on.
 			int count = i; //Reps where it will be placed.
 			for(int j = 0; j < rotateAmount;++j) { // Control the count on where the number will be placed.
@@ -18,7 +13,7 @@ class rotate {
 					count = size - 1;
 				}
 			}
-			rotated[count] = temp1[i];
+			rotated[count] = Integer.parseInt(temp[i]);
 		}
 		return rotated;
 	}
@@ -31,8 +26,8 @@ public class Solution {
         int rotations = scan.nextInt();
         scan.nextLine();
         String nums = scan.nextLine();
-		for(int i = 0; i < size;++i){
-			System.out.print(rotate.left(nums, rotations, size)[i] + " ");
-		}
+        System.out.println(Arrays.toString(rotate.left(nums, rotations, size)).replace("[", "")
+        		                                                              .replace(",", "")
+        		                                                              .replace("]", ""));
 	}
 }
