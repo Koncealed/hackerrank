d3a59b5f3a0b3a28cd89da8d789d27ac8abd3068
Robbie Skonieczny <robbieskonieczny@gmail.com>
2017-06-07 11:45:28 -0400

    Update Recursion(Factorial).java

--- Recursion(Factorial).java
+++ Recursion(Factorial).java
@@ -1,20 +1,15 @@
-import java.util.Scanner; //https://www.hackerrank.com/challenges/30-recursion
-class factorial {
-    
-    public static int factorial(int factAmount) {
-        int result = factAmount;
-        for(int i = factAmount - 1; i > 0;--i) {
-            result *= i;
-        }
-        return result;
-    }
-}
-
-public class Solution {
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int factAmount = scan.nextInt();
-        System.out.println(factorial.factorial(factAmount));
-    }
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class Solution {
+	public static int fact(int n) {
+		if(n == 1) {
+			return 1;
+		} else {
+			return n * fact(n - 1);
+		}
+	}
+	public static void main (String[] args){
+		System.out.println(fact(5));
+	}
 }
